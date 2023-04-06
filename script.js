a6a771d0ca8c7b3c1c95c85b3cc84b29e0b97404
Joaquin Talice <joaquintalice@gmail.com>
2023-04-06 18:04:00 -0300

    Math practice

--- script.js
+++ script.js
@@ -31,12 +31,18 @@ let addingDecimal = decimal1 + decimal2;
 console.log(addingDecimal)
 
 /* test isNaN() global js function */
-
 let numOne = 200
 let numTwo = "2"
 
 let isOrNot = numOne / numTwo
 
+console.log(typeof(isOrNot))
 console.log(isNaN(isOrNot))
+/* test isNaN() global js function */
 
+let testInf = 10
 
+while (testInf != 15) {
+    testInf = testInf++
+    console.log(testInf);
+} 

600749180dce27b47f27d6cee9c53621a8720742
Joaquin Talice <joaquintalice@gmail.com>
2023-04-17 22:03:24 -0300

    Capitalize function modified

--- script.js
+++ script.js
@@ -7,15 +7,7 @@ function multiply(n1, n2) {
 }
 
 function capitalize(string) {
-
-    let convertString = string.toLowerCase();
-    let allTheString = convertString.slice(1);
-    let capitalLetter = convertString.slice(0, 1);
-    let capitalString = capitalLetter.toUpperCase();
-
-
-    return (`${capitalString}${allTheString}`)
-
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
 function lastLetter(string) {

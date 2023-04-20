d38fca847b684e680d8db56e431f2c43b333ca4b
Joaquin Talice <joaquintalice@gmail.com>
2023-04-19 22:19:01 -0300

    New function added. JaP Exercise

--- script.js
+++ script.js
@@ -35,3 +35,41 @@ function fbGame() {
         }
     }
 }
+
+
+
+let info = [
+    {situacion: "Entrega informe", cuadrante:1},    //Urgente e Importante
+    {situacion: "Ir al medico", cuadrante:1},       //Urgente e Importante
+    {situacion: "Recargar boletera", cuadrante:1}, // Urgente e Importante
+
+    {situacion: "Comprar papel higienico", cuadrante:2},   // Urgente y no importante
+    {situacion: "Pasear al perro", cuadrante:2},   // Urgente y no importante
+    {situacion: "Lavar la ropa", cuadrante:2},   // Urgente y no importante
+
+    {situacion: "Limpiar la casa", cuadrante:3},   // Importante y no urgente
+    {situacion: "Hacer mandados", cuadrante:3},   // Importante y no urgente
+    {situacion: "Pagar la cuenta de OSE", cuadrante:3},   // Importante y no urgente
+
+    {situacion: "Jugar videojuegos", cuadrante:4},   // No importante y no urgente
+    {situacion: "Mirar netflix", cuadrante:4},   // No importante y no urgente
+    {situacion: "Cortar el pasto", cuadrante:4},   // No importante y no urgente
+];
+
+
+function segundoEDA(situaciones) {
+
+    let sit = situaciones.toUpperCase(); // Lo que el usuario digita, se pasa a mayúscula
+
+    for(let i = 0 ; i < info.length ; i++ ) { // Se itera por el largo de info, o sea, 12 veces, partiendo desde 0
+        if(sit === info[i].situacion.toUpperCase() ) { //Si lo que digita el usuario es igual a la situacion del respectivo elemento de info, retorna verdadero
+            return info[i].cuadrante; // Si es verdadero, se ejecuta el cuadrante del respectivo elemento de info
+        }
+    }
+    
+    return 0; // Si no es nada de lo que digita el usuario, retorna 0
+}
+
+
+
+
